ab0ad81ef8d56bb25d7414f874fe70a5bd83fa3b
fjhansen <58237856+fjhansen@users.noreply.github.com>
2020-05-05 15:54:49 -0700

    get

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const db = require("../data/db");
 const router = express.Router()
 
+// GET
 router.get('/', (req, res)  => {
   db.find()
   .then(posts => {
@@ -15,9 +16,27 @@ router.get('/', (req, res)  => {
   })
 })
 
-// router.get('/api/posts', (req, res) => {
-//   db.find()
-//   .then(posts => )
-// })
+router.get('/:id', (req, res) => {
+  db.findById(req.params.id)
+  .then(posts => {
+    if (posts) {
+      res.status(200).json(posts)
+    } else {
+      res.status(404).json({message: 'The post information could not be retrieved'})
+    }
+  })
+})
+
+router.get('/:id/comments', (req, res) => {
+  db.findCommentById(req.params.id)
+  .then(posts => {
+    if (posts) {
+      res.status(200).json(posts)
+    } else {
+      res.status(404).json({message: 'The post with the specified ID does not exist.'})
+    }
+  })
+})
+
 
 module.exports = router
